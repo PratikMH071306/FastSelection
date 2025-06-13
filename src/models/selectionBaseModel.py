9958b7b7d07a20f588cf13c368545ebc6bbc65f6
pratikVelo25 <pratik.raorane@velotechsolutions.com>
2025-06-13 13:20:18 +0530

    first commit

--- src/models/selectionBaseModel.py
+++ src/models/selectionBaseModel.py
@@ -1,70 +1,70 @@
 
-# from pydantic import BaseModel
-# from typing import Optional
+from pydantic import BaseModel
+from typing import Optional
 
-# class PumpDataBase(BaseModel):
-#     id: int
-#     pumpModel: str
-#     pumpTypeUuid: str
-#     pumpModelUuid: str
-#     primeMoverUuid: str
-#     perfCurvNo: str
-#     pumpModelId: int
-#     perfCurvNoId: int
-#     series: str
-#     pumpTypeId: int
-#     stgType: str
-#     stg: int
+class PumpDataBase(BaseModel):
+    id: int
+    pumpModel: str
+    pumpTypeUuid: str
+    pumpModelUuid: str
+    primeMoverUuid: str
+    perfCurvNo: str
+    pumpModelId: int
+    perfCurvNoId: int
+    series: str
+    pumpTypeId: int
+    stgType: str
+    stg: int
 
-#     trimDia: float
-#     maxDia: float
-#     minDia: float
-#     diaList: str
-#     diaList2: str
-#     suction: str
-#     discharge: str
-#     pumpShaftDia: str
-#     shaftGroup: str
-#     impellerDia2: str
-#     recommendedPipeSuc: int
-#     recommendedPipeDis: int
+    trimDia: float
+    maxDia: float
+    minDia: float
+    diaList: str
+    diaList2: str
+    suction: str
+    discharge: str
+    pumpShaftDia: str
+    shaftGroup: str
+    impellerDia2: str
+    recommendedPipeSuc: int
+    recommendedPipeDis: int
 
-#     speed: int
-#     pole: int
-#     trimRatio: float
-#     bepPerPumpWater: str
-#     bepPerPump: str
-#     bepWater: str
-#     bepPump: str
-#     bepEff: float
-#     bepFlowPercentage: float
-#     minContinuousFlow: float
-#     operatingRange: str
+    speed: int
+    pole: int
+    trimRatio: float
+    bepPerPumpWater: str
+    bepPerPump: str
+    bepWater: str
+    bepPump: str
+    bepEff: float
+    bepFlowPercentage: float
+    minContinuousFlow: float
+    operatingRange: str
 
-#     efficiencyDp: str
-#     npshrDp: str
-#     powerDp: str
-#     powerDpMax: str
-#     soh: str
+    efficiencyDp: str
+    npshrDp: str
+    powerDp: str
+    powerDpMax: str
+    soh: str
 
-#     primeMoverID: int
-#     primeMoverPower: str
-#     primeMoverFrame: str
-#     primeMoverPowerHP: str
-#     primemoverSpeed: str
+    primeMoverID: int
+    primeMoverPower: str
+    primeMoverFrame: str
+    primeMoverPowerHP: str
+    primemoverSpeed: str
 
-#     system: str
-#     noOfPumps: int
-#     staticHead: int
-#     verticalLogic: bool
-#     genericBom: Optional[bool] = None
-#     patternAvl: Optional[bool] = None
-#     discontinued: Optional[bool] = None
+    system: str
+    noOfPumps: int
+    staticHead: int
+    verticalLogic: bool
+    genericBom: Optional[bool] = None
+    patternAvl: Optional[bool] = None
+    discontinued: Optional[bool] = None
 
-#     specificSpeed: float
-#     suctionSpecificSpeed: float
-#     tipSpeedCal: float
-#     sysPreCal: str
+    specificSpeed: float
+    suctionSpecificSpeed: float
+    tipSpeedCal: float
+    sysPreCal: str
 
-# #   class Config:
-# #       extra = Extra.allow  # Allow unexpected fields
+#   class Config:
+#       extra = Extra.allow  # Allow unexpected fields
